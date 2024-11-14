godotengine/godot

Added Breakpad crash dump generation
Fixed compiling on Windows

Make crash dump printing work better on Windows

Tweaked Breakpad build

Fixed Breakpad build for Godot 4

Switched to WINDOWS_ENABLED

Removed some non buildable files listed in the files to build

Ran clang format

Fixed the other formatting issues detected by CI

Removed a comment and added clarifying comment on crash dump message

as to why it is printed twice on Windows

Make an ugly string conversion to make Windows build work

Tweaked the build configuration and formatted again

removed lss

Add lss properly

Reinitialize breakpad after mono initialization on Linux

otherwise the breakpad signal handlers are not active

Disable Windows crash handler on destruction similarly to Linux

Renamed breakpad_enabled to use_breakpad

Forgot to wrap one piece of code inside ifdef USE_BREAKPAD

Updated copyright years in the added files

Fix register types for breakpad

Fixed dir access

Removed musl and elf_reader

which was the only thing seemingly depending on it

Updated header guards

Removed the memdelete call

SConstruct
@@ -224,6 +224,7 @@ opts.Add(BoolVariable("use_volk", "Use the volk library to load the Vulkan loade
 opts.Add(BoolVariable("disable_exceptions", "Force disabling exception handling code", True))
 opts.Add("custom_modules", "A list of comma-separated directory paths containing custom modules to build.", "")
 opts.Add(BoolVariable("custom_modules_recursive", "Detect custom modules recursively for each specified path.", True))
+opts.Add(BoolVariable("use_breakpad", "Enable Breakpad crash dump creation.", False))
 
 # Advanced options
 opts.Add(BoolVariable("dev_mode", "Alias for dev options: verbose=yes warnings=extra werror=yes tests=yes", False))
@@ -521,6 +522,9 @@ if not env["deprecated"]:
 
 if env["precision"] == "double":
     env.Append(CPPDEFINES=["REAL_T_IS_DOUBLE"])
+
+if env["use_breakpad"]:
+    env_base.Append(CPPDEFINES=["USE_BREAKPAD"])
 
 tmppath = "./platform/" + env["platform"]
 sys.path.insert(0, tmppath)

mk56-spn/shinobu

SConstruct
@@ -229,6 +229,7 @@ opts.Add(BoolVariable("disable_exceptions", "Force disabling exception handling 
 opts.Add("custom_modules", "A list of comma-separated directory paths containing custom modules to build.", "")
 opts.Add(BoolVariable("custom_modules_recursive", "Detect custom modules recursively for each specified path.", True))
 opts.Add(BoolVariable("swappy", "Use Swappy Frame Pacing Library in Android builds.", False))
+opts.Add(BoolVariable("use_breakpad", "Enable Breakpad crash dump creation.", False))
 
 # Advanced options
 opts.Add(
@@ -527,6 +528,9 @@ if not env["deprecated"]:
 
 if env["precision"] == "double":
     env.Append(CPPDEFINES=["REAL_T_IS_DOUBLE"])
+
+if env["use_breakpad"]:
+    env.Append(CPPDEFINES=["USE_BREAKPAD"])
 
 tmppath = "./platform/" + env["platform"]
 sys.path.insert(0, tmppath)